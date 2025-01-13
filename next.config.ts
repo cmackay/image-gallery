dd690ba3a8b05c9ac100d2d484fedd0c8e87dfe6
Craig MacKay <craig2600@gmail.com>
2025-01-12 18:33:42 -0800

    add postcss extension and fix some lint issues

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["i.imgur.com"],
+    domains: ['i.imgur.com'],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
